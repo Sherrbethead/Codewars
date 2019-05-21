71721bc09cc3d530ddf1efc1e3273e3b371b774f
sherrbethead <bakanovsergs@gmail.com>
2019-05-22 00:08:20 +0300

    first commit

---
+++ regexpasswordvalidation.py
@@ -0,0 +1,15 @@
+'''Description
+
+You need to write regex that will validate a password to make sure it meets the following criteria:
+- At least six characters long;
+- Contains a lowercase letter;
+- Contains an uppercase letter;
+- Contains a number.
+Valid passwords will only be alphanumeric characters.'''
+
+from re import search
+
+regex = '^[^_\W)]*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[^_\W)]*$'
+
+print(bool(search(regex, 'fjd3IR9')))  # True
+print(bool(search(regex, 'DSJKHD23')))  # False
